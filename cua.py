a622a92a3eac1a7676b8431ae2e641b753bc3b99
laygofiona <laygo.fiona@gmail.com>
2025-09-13 16:56:11 -0400

    Adding instructions to determine the instrument and select instrument

--- cua.py
+++ cua.py
@@ -3,9 +3,10 @@ import asyncio
 import logging
 from pathlib import Path
 from agent import ComputerAgent
+import os
 
 
-async def computer_use_agent(midi_file='./hum_basic_pitch.mid', instrument='piano'):
+async def computer_use_agent(midi_file='./hum_basic_pitch.mid', instrument='guitar'):
     """
     Main async function that handles the computer automation.
     This function:
@@ -26,7 +27,7 @@ async def computer_use_agent(midi_file='./hum_basic_pitch.mid', instrument='pian
         os_type="linux",                    # Using Linux environment
         provider_type="cloud",              # Cloud-based computer
         name="l-linux-x318jdeu72",         # Specific instance name
-        api_key=""
+        api_key=os.getenv('CUA_API_KEY')
     ) as computer:
         print("🖥️ Connected to cloud computer")
         
@@ -46,7 +47,7 @@ async def computer_use_agent(midi_file='./hum_basic_pitch.mid', instrument='pian
             max_trajectory_budget=5.0
         )
         tasks = [f"""
-You are inside BandLab Studio in Firefox on Linux.
+You are inside BandLab Studio in Firefox on Linux. p
 
 Goal: Import the MIDI file and play it with the chosen instrument.
 
@@ -55,7 +56,7 @@ FILE NAME ONLY:
 INSTRUMENT: {instrument}
 
 Do the following step by step:
-
+   
 1. Click the dashed box in the timeline that says “Drop a loop or an audio/MIDI/video file”.
    - This should open a file upload dialog.
 
@@ -65,6 +66,15 @@ Do the following step by step:
    - If it’s not visible, type “{midi_name}” into the filename field and press Enter.
    - Wait until the MIDI region appears on the timeline.
    - Take a screenshot.
+   
+3. Click the "Instrument" button in the bottom left corner. It is the button with the text "Instrument" and an icon of a piano.
+
+4. Click the grey button with text "Grand Piano". Wait for a pop up called Browse Instruments to show up. 
+
+5. In the search bar, type in the {instrument} name. Select the first clickable option in the list in the pop up. Click the "Instrument" button in the bottom left corner.
+
+
+6. Click the long white cursor and drag it to the beginning.
 
 Rules:
 - Always interact inside BandLab, not the browser’s URL bar.

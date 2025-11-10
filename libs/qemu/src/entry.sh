c59aa520df151076adab4f23b42a4e6fd49caf5e
synacktra.work@gmail.com <synacktra.work@gmail.com>
2025-11-10 14:32:56 +0530

    chore(qemu): remove unnecessary comments

--- libs/qemu/src/entry.sh
+++ libs/qemu/src/entry.sh
@@ -16,17 +16,14 @@ echo "Live stream accessible at localhost:8006"
 
 echo "Waiting for Windows to boot and CUA computer-server to start..."
 while true; do
-  # Send a GET request to check if server is ready
+  # Check if server is ready
   response=$(curl --write-out '%{http_code}' --silent --output /dev/null 20.20.20.21:5000/status)
 
-  # If the response code is 200 (HTTP OK), break the loop
   if [ "${response:-0}" -eq 200 ]; then
     break
   fi
 
   echo "Waiting for CUA computer-server to be ready. This might take a while..."
-
-  # Wait for a while before the next attempt
   sleep 5
 done
 
@@ -39,8 +36,7 @@ socat TCP-LISTEN:5000,fork,reuseaddr TCP:20.20.20.21:5000 &
 echo "Computer server accessible at localhost:5000"
 
 # Detect initial setup by presence of /custom.iso (setup ISO mount)
-if [ ! -f "/custom.iso" ]; then
-  # Keep container alive
+if [ ! -f "/custom.iso" ]; then # Keep container alive
   echo "Container running. Press Ctrl+C to stop."
   tail -f /dev/null
 fi

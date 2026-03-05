f4d68ff0c4ba6ea86628a87b6f8d8bae21a39e07
r33drichards <rwendt1337@gmail.com>
2026-03-05 07:49:53 -0800

    Revert "feat: add optional VNC password authentication (#1072)"
    This reverts commit fd9cdedb44666f90fd9b5a1710c32c85766bdbab.

--- libs/xfce/src/scripts/start-vnc.sh
+++ libs/xfce/src/scripts/start-vnc.sh
@@ -4,30 +4,20 @@ set -e
 # Clean up any existing VNC lock files
 rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
 
-# Set up VNC authentication if VNC_PW is provided
-if [ -n "$VNC_PW" ]; then
-    mkdir -p /home/cua/.vnc
-    echo "$VNC_PW" | vncpasswd -f > /home/cua/.vnc/passwd
-    chmod 600 /home/cua/.vnc/passwd
-    SECURITY_ARGS="-SecurityTypes VncAuth -rfbauth /home/cua/.vnc/passwd"
-    unset VNC_PW
-else
-    SECURITY_ARGS="-SecurityTypes None --I-KNOW-THIS-IS-INSECURE"
-fi
-
-# Start VNC server
+# Start VNC server without password authentication
 vncserver :1 \
     -geometry ${VNC_RESOLUTION:-1920x1080} \
     -depth ${VNC_COL_DEPTH:-24} \
     -rfbport ${VNC_PORT:-5901} \
     -localhost no \
-    $SECURITY_ARGS \
+    -SecurityTypes None \
     -AlwaysShared \
     -AcceptPointerEvents \
     -AcceptKeyEvents \
     -AcceptCutText \
     -SendCutText \
-    -xstartup /usr/local/bin/xstartup.sh
+    -xstartup /usr/local/bin/xstartup.sh \
+    --I-KNOW-THIS-IS-INSECURE
 
 # Keep the process running
 tail -f /home/cua/.vnc/*.log

daf678348b01506d80ca0c1af5d94aee5b843e0d
Samsen879 <guoqiyang25@gmail.com>
2026-06-10 10:28:22 +0800

    fix(computer): align TypeScript host desktop docs

--- libs/typescript/computer/src/index.ts
+++ libs/typescript/computer/src/index.ts
@@ -1,5 +1,13 @@
 // Export classes
 export { CloudComputer as Computer } from './computer';
+export {
+  BaseComputerInterface,
+  InterfaceFactory,
+  LinuxComputerInterface,
+  MacOSComputerInterface,
+  WindowsComputerInterface,
+} from './interface';
+export type { AccessibilityNode, CursorPosition, MouseButton } from './interface';
 
 export { OSType } from './types';
 export { VMProviderType as ProviderType } from './computer/types';

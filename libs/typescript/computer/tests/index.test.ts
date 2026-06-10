446cec06d59b93b4bb08a78a7da22fb35ec0b3be
Samsen879 <guoqiyang25@gmail.com>
2026-06-10 22:42:30 +0800

    docs(computer): clarify TypeScript host operations

--- libs/typescript/computer/tests/index.test.ts
+++ libs/typescript/computer/tests/index.test.ts
@@ -3,6 +3,8 @@ import * as ComputerExports from '../src/index.ts';
 
 describe('package exports', () => {
   it('exports host computer interfaces from the package root', () => {
+    expect(ComputerExports.BaseComputerInterface).toBeDefined();
+    expect(ComputerExports.InterfaceFactory).toBeDefined();
     expect(ComputerExports.MacOSComputerInterface).toBeDefined();
     expect(ComputerExports.LinuxComputerInterface).toBeDefined();
     expect(ComputerExports.WindowsComputerInterface).toBeDefined();

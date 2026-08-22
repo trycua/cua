30b6846e4f501c09db524273962f168b36c7171c
r33drichards <57335981+r33drichards@users.noreply.github.com>
2026-08-22 03:09:42 +0000

    feat(sandbox): emit validated Image resources

--- libs/python/cua-sandbox/cua_sandbox/__init__.py
+++ libs/python/cua-sandbox/cua_sandbox/__init__.py
@@ -20,6 +20,7 @@ __version__ = "0.1.0"
 
 from cua_sandbox._auth import login, whoami
 from cua_sandbox._config import configure
+from cua_sandbox.generated.image_models import ImageFileReference
 from cua_sandbox.image import Image
 from cua_sandbox.localhost import Localhost, localhost
 from cua_sandbox.pool import Pool, Template
@@ -62,6 +63,7 @@ __all__ = [
     "login",
     "whoami",
     "Image",
+    "ImageFileReference",
     "Pool",
     "PoolAccessDeniedError",
     "Template",
